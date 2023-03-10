42d20381dbca0b484c7cfe56aef31153dd438f13
tioibiy <1092324959@qq.com>
2023-03-10 13:06:32 +0800

    modified:   Code/Python/draw.py 	modified:   Code/Python/draw.py

--- Code/Python/draw.py
+++ Code/Python/draw.py
@@ -17,19 +17,19 @@ for inf in range(4,1596):
 for inf in range(1596,3188):
     end.append([float(xyz[inf].split()[1]),float(xyz[inf].split()[3]),float(xyz[inf].split()[2])])
 
-fig = plt.figure()
+# fig = plt.figure()
 
-ax = fig.add_axes(Axes3D(fig))
+# ax = fig.add_axes(Axes3D(fig))
 
-# ax.set_zlim3d(0,12)
+# # ax.set_zlim3d(0,12)
 
-# print(start[1],end[1])
+# # print(start[1],end[1])
 
-for i in range(1596-4):
-    ax.quiver(start[i][0],start[i][1],start[i][2],end[i][0],end[i][1],end[i][2],arrow_length_ratio=0.1)
+# for i in range(1596-4):
+#     ax.quiver(start[i][0],start[i][1],start[i][2],end[i][0],end[i][1],end[i][2],arrow_length_ratio=0.1)
 
 
-plt.show()
+# plt.show()
 # fig = plt.figure()  
 # ax = plt.axes(projection='3d')
 # plt.axis('off')
